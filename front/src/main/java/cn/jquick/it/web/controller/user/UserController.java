4c24c8d5fe2e747fcb75ca517c21946ac0bbf66f
xinou <xinou28@gmail.com>
2016-03-19 00:08:04 +0800

    修改jar包配置

--- front/src/main/java/cn/jquick/it/web/controller/user/UserController.java
+++ front/src/main/java/cn/jquick/it/web/controller/user/UserController.java
@@ -1,7 +1,21 @@
 package cn.jquick.it.web.controller.user;
 
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import cn.jquick.it.common.model.user.table.TUser;
 
 /** 
  * 用户管理控制器
@@ -14,14 +28,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController
 {
-    @RequestMapping("/login")
-    public String login()
+    /** 
+     *跳转登录页面 
+     *<功能详细描述>
+     * @return 
+     */
+    //@RequestMapping("/login.html")
+    public String showLogin()
     {
-        System.err.println("111111111111111");
+        System.err.println("login.html");
         return "user/login";
     }
     
-    @RequestMapping("/register")
+    /** 
+     *用户登录 
+     *<功能详细描述>
+     * @return 
+     */
+    @Produces(MediaType.APPLICATION_JSON)
+    @ResponseBody
+    @RequestMapping(value = "/login.do", method = RequestMethod.POST)
+    public Map<String, String> login()
+    {
+        System.err.println("login.do");
+        Map<String, String> map = new HashMap<String,String>();
+        map.put("a", "11111");
+        return map;
+    }
+    
+    /** 
+     *跳转注册页面 
+     *<功能详细描述>
+     * @return 
+     */
+    @RequestMapping("/register.html")
     public String showRegister()
     {
         return "user/register";
